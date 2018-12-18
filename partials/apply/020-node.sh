2e2bc471b23c3774aa62c4ddf1c19d22e7d5de26
Liam Dawson <liam@ldaws.com>
2018-12-19 10:22:58 +1100

    update nodenv plugins

--- partials/apply/020-node.sh
+++ partials/apply/020-node.sh
@@ -1,12 +1,33 @@
-if [[ ! -d "${NODENV_ROOT}" ]]
-then
-  git clone https://github.com/nodenv/nodenv.git "${NODENV_ROOT}"
-  git clone https://github.com/nodenv/node-build.git "${NODENV_ROOT}/plugins/node-build"
-  git clone https://github.com/nodenv/nodenv-update.git "${NODENV_ROOT}/plugins/nodenv-update"
-  git clone https://github.com/nodenv/nodenv-default-packages.git "${NODENV_ROOT}/plugins/nodenv-default-packages"
-fi
+#!/usr/bin/env bash
 
-cat << EOF > "${NODENV_ROOT}/default-packages"
+(
+  set -e
+  set -u
+
+  function install_plugin() {
+    [[ -d "${NODENV_ROOT}/plugins" ]] || mkdir "${NODENV_ROOT}/plugins"
+
+    pushd "${NODENV_ROOT}/plugins"
+
+    if [[ ! -d "$2" ]]
+    then
+      git clone "$1" "$2"
+    fi
+
+    popd
+  }
+
+  if [[ ! -d "${NODENV_ROOT}" ]]
+  then
+    git clone https://github.com/nodenv/nodenv.git "${NODENV_ROOT}"
+  fi
+
+  install_plugin https://github.com/nodenv/node-build.git "node-build"
+  install_plugin https://github.com/nodenv/nodenv-update.git "nodenv-update"
+  install_plugin https://github.com/nodenv/nodenv-default-packages.git "nodenv-default-packages"
+  install_plugin https://github.com/nodenv/nodenv-package-rehash.git "nodenv-package-rehash"
+
+  cat << EOF > "${NODENV_ROOT}/default-packages"
 npm
 yarn
 eslint
@@ -17,5 +38,6 @@ webpack
 emoj
 EOF
 
-echo '8.12.0' > "${NODENV_ROOT}/version"
-nodenv install -s "$(cat ${NODENV_ROOT}/version)"
+  echo '8.12.0' > "${NODENV_ROOT}/version"
+  nodenv install -s "$(cat ${NODENV_ROOT}/version)"
+)
