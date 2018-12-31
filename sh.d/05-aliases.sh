306dc74debb76b8ada834cb8f8dc8786c94fd901
Liam Dawson <liam@ldaws.com>
2018-12-31 12:24:55 +1100

    add op alias function

--- sh.d/05-aliases.sh
+++ sh.d/05-aliases.sh
@@ -2,3 +2,11 @@
 
 alias docs.rs="cargo doc --open --no-deps -p"
 (! command -v 'direnv' >/dev/null 2>&1) || alias dx="direnv exec ."
+
+if command -v 'op' >/dev/null 2>&1
+then
+  function ope() {
+    eval "$(op signin $1)"
+  }
+fi
+
