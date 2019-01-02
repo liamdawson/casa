d003c07b49af4d56a707e15d27b1d8f33876221b
Liam Dawson <liam@ldaws.com>
2019-01-02 23:41:43 +1100

    add expected rust components

--- partials/apply/020-rust.sh
+++ partials/apply/020-rust.sh
@@ -1,4 +1,6 @@
-if ! which rustup >/dev/null 2>&1; then
+#!/usr/bin/env bash
+
+if ! command -v rustup >/dev/null 2>&1; then
   curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
 fi
 
@@ -7,3 +9,8 @@ fi
 [[ -r "${HOME}/.rustup" ]] || ln -s "${XDG_DATA_HOME}/rustup" "${HOME}/.rustup"
 [[ -r "${HOME}/.multirust" ]] || ln -s "${XDG_DATA_HOME}/rustup" "${HOME}/.multirust"
 
+# expected components
+rustup component list | grep -q 'rust-src.*\(installed\)' || rustup component add rust-src
+rustup component list | grep -q 'clippy.*\(installed\)' || rustup component add clippy
+rustup component list | grep -q 'rustfmt.*\(installed\)' || rustup component add rustfmt
+rustup component list | grep -q 'rls.*\(installed\)' || rustup component add rls
